a05237a4cc52f959ae875834950402fb62d74106
unknown <80556065+Smart-Bot-Universe@users.noreply.github.com>
2022-09-13 15:57:37 -0700

    Added Ball class, made bricks shows, more framework

--- BrickBreaker.cpp
+++ BrickBreaker.cpp
@@ -3,11 +3,17 @@
 
 class BrickBreaker : public olc::PixelGameEngine
 {
+	enum BrickStructureShape
+	{
+		SQUARE, RECTANGLE, CIRCLE, TRIANGLE
+	};
+
 	class Brick
 	{
 		public:
 			float x, y, width, height;
 			olc::Pixel color;
+			int lives;
 
 			Brick(float x, float y, float width, float height, olc::Pixel color)
 			{
@@ -16,12 +22,42 @@ class BrickBreaker : public olc::PixelGameEngine
 				this->width = width;
 				this->height = height;
 				this->color = color;
+
+				lives = 3;
+			}
+
+			void removeALife()
+			{
+				lives--;
+			}
+	};
+
+	class Ball
+	{
+		public:
+			olc::vf2d velocity;
+			float x, y, radius;
+
+			Ball(float x, float y, float radius, olc::vf2d velocity)
+			{
+				this->x = x;
+				this->y = y;
+				this->radius = radius;
+				this->velocity = velocity;
+			}
+
+			bool Collides(Brick b)
+			{
+				// The ball doesn't exist.
+				return false;
 			}
 	};
 
 	public:
 		std::vector<Brick> bricks;
 		int brickAmt;
+
+		int brick_width, brick_height;
 
 		BrickBreaker()
 		{
@@ -30,12 +66,57 @@ class BrickBreaker : public olc::PixelGameEngine
 		
 		bool OnUserCreate() override
 		{
+			brick_width = 30;
+			brick_height = 10;
+
+			brickAmt = 10;
+
+			GenerateBricks();
 			return true;
 		}
 
 		bool OnUserUpdate(float fElapsedTime) override
 		{
+			for (int i = 0; i < bricks.size(); i++)
+			{
+				Brick b = bricks[i];
+
+				// Collision with the ball
+
+				if (b.lives == 0)
+				{
+					bricks.erase(bricks.begin() + i);
+					continue;
+				}
+
+				FillRect(b.x, b.y, b.width, b.height, b.color);
+			}
 			return true;
+		}
+
+		void GenerateBricks()
+		{
+			int length = brickAmt >> 1;
+			olc::Pixel color;
+
+			for (int x = 0; x < length; x++)
+			{
+				for (int y = 0; y < length; y++)
+				{
+					color = olc::Pixel(rand() % 155 + 100, rand() % 155 + 100, rand() % 155 + 100);
+					// Those threes is added there so as to see how each brick is different from eachother. 
+					// Prob will be removed if I do borders, sprites, etc.
+
+					// First paranthesis is the offset from the gui border, second is the calculating where they are, and third is the offset from each other.
+					bricks.push_back(
+						*new Brick(
+							(brick_width) + (x * brick_width) + (1 * x), 
+							(brick_height * 2) + (y * brick_height) + (1 * y), 
+							brick_width, brick_height, color
+						)
+					);
+				}
+			}
 		}
 };
 
