508f529d9cd53b632a6294a10caab1f39658fe09
unknown <80556065+Smart-Bot-Universe@users.noreply.github.com>
2022-09-13 22:25:35 -0700

    Movement for player and ball.

--- BrickBreaker.cpp
+++ BrickBreaker.cpp
@@ -1,5 +1,4 @@
-#define OLC_PGE_APPLICATION
-#include "../vendor/olcPixelGameEngine/olcPixelGameEngine.h"
+#include "GameEngine.h"
 
 class BrickBreaker : public olc::PixelGameEngine
 {
@@ -8,28 +7,41 @@ class BrickBreaker : public olc::PixelGameEngine
 		SQUARE, RECTANGLE, CIRCLE, TRIANGLE
 	};
 
-	class Paddle
+	class Paddle : public shape::Rectangle
 	{
-		public:
-			float x, y, width, height;
-			olc::Pixel color;
+		private:
+			float velocity;
+			olc::Key left, right;
 
-			Paddle(float x, float y, float width, float height, olc::Pixel color)
+			Paddle(float x, float y, float width, float height, olc::Pixel color, olc::Key left, olc::Key right) : shape::Rectangle(x, y, width, height, color)
 			{
-				this->x = x;
-				this->y = y;
-				this->width = width;
-				this->height = height;
-				this->color = color;
+				velocity = 10.0f;
+
+				this->left = left;
+				this->right = right;
+			}
+
+			void OnUserUpdate(float fElapsedTime, BrickBreaker *b)
+			{
+				if (b->GetKey(left).bHeld && x > 0)
+				{
+					x -= velocity * fElapsedTime;
+				}
+				else if (b->GetKey(right).bHeld && x < b->ScreenWidth())
+				{
+					x += velocity * fElapsedTime;
+				}
+
+				b->FillRect(x, y, width, height, color);
 			}
 	};
 
-	class Brick : public Paddle
+	class Brick : public shape::Rectangle
 	{
 		public:
 			int lives;
 
-			Brick(float x, float y, float width, float height, olc::Pixel color) : Paddle(x, y, width, height, color)
+			Brick(float x, float y, float width, float height, olc::Pixel color) : shape::Rectangle(x, y, width, height, color)
 			{
 				lives = 3;
 			}
@@ -37,26 +49,56 @@ class BrickBreaker : public olc::PixelGameEngine
 			void removeALife()
 			{
 				lives--;
+				
 			}
 	};
 
-	class Ball
+	class Ball : public shape::Circle
 	{
 		public:
 			olc::vf2d velocity;
-			float x, y, radius;
 
-			Ball(float x, float y, float radius, olc::vf2d velocity)
+			Ball(float x, float y, float radius, olc::Pixel color, olc::vf2d velocity) : shape::Circle(x, y, radius, color)
 			{
-				this->x = x;
-				this->y = y;
-				this->radius = radius;
 				this->velocity = velocity;
 			}
 
-			bool Collides(Paddle p)
+			void OnUserUpdate(float fElapsedTime, BrickBreaker* b)
+			{
+				x += velocity.x * fElapsedTime;
+				y += velocity.y * fElapsedTime;
+
+				if (x < 0 || x > b->ScreenHeight())
+				{
+					velocity.x = -velocity.x;
+				}
+				if (y < 0)
+				{
+					velocity.y = -velocity.y;
+					y = radius / 2;
+				}
+				else if (y > b->ScreenHeight())
+				{
+					// GameOver or possibility just the removal of itself inside an std::vector<Ball>
+				}
+
+				// Figure out closest brick and then check collision with
+				Brick closestBrick = *new Brick(0, 0, 0, 0, olc::WHITE);
+				if (Collides(closestBrick))
+				{
+					closestBrick.removeALife();
+				}
+
+				b->FillCircle(x, y, radius, color);
+			}
+
+			bool Collides(shape::Rectangle p)
 			{
 				// The ball doesn't exist.
+				// Deal with the bounce here.
+				// Or just make two methods.
+				// Checking specific collisions.
+				// (Horizontal and Vertical)
 				return false;
 			}
 	};
@@ -66,6 +108,9 @@ class BrickBreaker : public olc::PixelGameEngine
 		int brickAmt;
 
 		int brick_width, brick_height;
+
+		Paddle* player;
+		Ball* ball;
 
 		BrickBreaker()
 		{
@@ -130,10 +175,10 @@ class BrickBreaker : public olc::PixelGameEngine
 
 int main()
 {
-	BrickBreaker brickBreacker;
-	if (brickBreacker.Construct(450, 300, 2, 2, false, true, false))
+	BrickBreaker brickBreaker;
+	if (brickBreaker.Construct(450, 300, 2, 2, false, true, true))
 	{
-		brickBreacker.Start();
+		brickBreaker.Start();
 	}
 	return 0;
 }
