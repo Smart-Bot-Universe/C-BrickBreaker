0ecdd24d044538a6e93ad2a21ff295e8ca72e27d
unknown <80556065+Smart-Bot-Universe@users.noreply.github.com>
2022-09-13 22:23:32 -0700

    Where the olc engine is and some helper classes

---
+++ GameEngine.h
@@ -0,0 +1,37 @@
+#pragma once
+#define OLC_PGE_APPLICATION
+#include "../vendor/olcPixelGameEngine/olcPixelGameEngine.h"
+
+namespace shape
+{
+	class Rectangle
+	{
+		public:
+			float x, y, width, height;
+			olc::Pixel color;
+
+			Rectangle(float x, float y, float width, float height, olc::Pixel color)
+			{
+				this->x = x;
+				this->y = y;
+				this->width = width;
+				this->height = height;
+				this->color = color;
+			}
+	};
+
+	class Circle
+	{
+		public:
+			float x, y, radius;
+			olc::Pixel color;
+
+			Circle(float x, float y, float radius, olc::Pixel color)
+			{
+				this->x = x;
+				this->y = y;
+				this->radius = radius;
+				this->color = color;
+			}
+	};
+}
